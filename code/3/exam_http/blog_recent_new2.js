c320bf68dfb7adb65fe9699b87195d662a82be2e
dongxiaochen <dongxiaochen@adintl.cn>
2019-05-07 14:48:14 +0800

    解除嵌套 并简化if..else...为return

---
+++ code/3/exam_http/blog_recent_new2.js
@@ -0,0 +1,34 @@
+// 将json数据渲染到html的逻辑 解除嵌套 并简化if..else...为return
+
+var http = require('http')
+var fs = require('fs')
+
+var server = http.createServer(fucntion(req,res) {
+    //控制权转交给了getTitles
+    getTitles(res)
+}).listen(8000,'127.0.0.1')
+// 获取标题，并将控制权转交给getTemplate
+function getTitles(res) {
+    fs.readFile('./titles.json',function(err,data){
+        if(err) return hadError(err,res)
+        getTemplate(JSON.parse(data.toString()),res)
+    })
+}
+// getTemplate读取模板文件，并将控制权转交给formatHtml
+function getTemplate(titles,res){
+    fs.readFile('./template.html',function(err,data) {
+        if(err) return hadError(err,res)
+        formatHtml(titles,data.toString(),res)
+    }
+}
+// formatHtml得到标题和模板，渲染一个响应给客户端
+function formatHtml(titles,tmpl,res) {
+    var html = tmpl.replace('%',titles.join('</li><li>'))
+    res.writeHead(200,{'Content-Type':'text/html'})
+    // 发送给客户端
+    res.end(html)
+}
+function hadError(err,res) {
+    console.error(err)
+    res.end('errrrrrr')
+}
