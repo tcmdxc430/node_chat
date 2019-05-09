375edac949fb8a8d085e13dd30ae55ae58867802
dongxiaochen <dongxiaochen@adintl.cn>
2019-05-09 11:02:25 +0800

    是用事件发射器创建新类

--- code/3/event_emitter_extend.js
+++ code/3/event_emitter_extend.js
@@ -11,19 +11,22 @@ util.inherits(Watcher, events.EventEmitter);
 var fs = require('fs')
   , watchDir = './watch'
   , processedDir  = './done';
-
+// 扩展EventEmitter，添加处理文件的方法
 Watcher.prototype.watch = function() { 
   var watcher = this;
+  // 保存对Watcher对象的引用，以便在回调函数readdir中使用
   fs.readdir(this.watchDir, function(err, files) {
     if (err) throw err;
     for(index in files) {
+      // 处 理 watch目录中的所有文件
       watcher.emit('process', files[index]); 
     }
   })
 }
-
+// 扩展EventEmitter，添加开始监控的方法
 Watcher.prototype.start = function() { 
   var watcher = this;
+  // watchFile监控watchDir
   fs.watchFile(watchDir, function() {
     watcher.watch();
   });
