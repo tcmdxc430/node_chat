b1b7581c54b9e4ec94f83a928edb5acf4c739740
dongxiaochen <dongxiaochen@adintl.cn>
2019-05-21 17:40:37 +0800

    对一个静态文件的请求

--- code/4/4.3.3.js
+++ code/4/4.3.3.js
@@ -1,8 +1,17 @@
+// 创建静态文件服务器 并完善错误callback
+var http = require('http');
+var parse = require('url').parse;
+var join = require('path').join;
+var fs = require('fs');
+// 是该文件所在目录的路径
+var root = __dirname;
 var server = http.createServer(function(req, res){
   var url = parse(req.url);
   var path = join(root, url.pathname);
+  // 检查文件是否存在
   fs.stat(path, function(err, stat){
     if (err) {
+      // 文件没找到code
       if ('ENOENT' == err.code) {
         res.statusCode = 404;
         res.end('Not Found');
@@ -21,3 +30,4 @@ var server = http.createServer(function(req, res){
     }
   });
 });
+server.listen(3000, '127.0.0.1')
--- code/4/readstream_static.js
+++ code/4/readstream_static.js
@@ -1,20 +1,30 @@
+// 创建静态文件服务器
 var http = require('http');
 var parse = require('url').parse;
 var join = require('path').join;
 var fs = require('fs');
-
+// 是该文件所在目录的路径
 var root = __dirname;
 
 var server = http.createServer(function(req, res){
   var url = parse(req.url);
+  // 将pathname拼接到root后
   var path = join(root, url.pathname);
+  
   var stream = fs.createReadStream(path);
-  stream.on('data', function(chunk){
-    res.write(chunk);
-  });
-  stream.on('end', function(){
-    res.end();
-  });
+  
+  stream.pipe(res)
+  stream.on('error',function(err) {
+    res.statusCode = 500
+    res.end('500')
+  })
+  // 将文件数据写到响应中
+  // stream.on('data', function(chunk){
+  //   res.write(chunk);
+  // });
+  // stream.on('end', function(){
+  //   res.end();
+  // });
 });
 
-server.listen(3000);
+server.listen(3000, '127.0.0.1')
