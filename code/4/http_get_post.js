226eddb19cf3bef8da932f4d7fdc1504d50a2f87
dongxiaochen <dongxiaochen@adintl.cn>
2019-05-22 14:43:44 +0800

    支持GET和POST的HTTP服务器

--- code/4/http_get_post.js
+++ code/4/http_get_post.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var items = [];
+var qs = require('querystring')
 
 var server = http.createServer(function(req, res){
+  console.log(req.url)
   if ('/' == req.url) {
     switch (req.method) {
       case 'GET':
@@ -18,4 +20,43 @@ var server = http.createServer(function(req, res){
   }
 });
 
-server.listen(3000);
+server.listen(3001);
+
+function show(res) {
+  var html = `<html>
+  <head><title>aa</title></head><body>
+      <h1>todolist</h1><ul>${items.map(function(item){return `<li>${item}</li>`}).join('')}</ul>
+      <form action="" method="POST"><p><input type="text" name="item"></p><p><input type="submit" value="add item"></p></form>
+  </body>
+</html>`
+  res.setHeader('Content-Type','text/html')
+  // 返回html字节长度
+  res.setHeader('Content-Length',Buffer.byteLength(html))
+  res.end(html)
+}
+
+function notFound(res) {
+  res.statusCode = 404
+  res.setHeader('Content-Type','text/plain')
+  res.end('not found')
+}
+function badRequest(res) {
+  res.statusCode = 400
+  res.setHeader('Content-Type','text/plain')
+  res.end('bad req')
+}
+
+function add(req,res) {
+  var body = ''
+  req.setEncoding('utf8')
+  req.on('data',function(chunk) {
+    body+=chunk
+  })
+  req.on('end',function() {
+    var obj = qs.parse(body)
+    
+    items.push(obj.item)
+    console.log(items)
+    show(res)
+  })
+}
